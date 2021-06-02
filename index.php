f96a3115a381922ed7ada1ccd5c9c1a7a37177e7
Projectneon <85230238+Projectneonstudio@users.noreply.github.com>
2021-06-02 14:36:43 +0100

    test upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Projectneonstudio</title>
+</head>
+
+<body>
+<div class="nav">
+	<p>sdfsdfsdfdfsfd </p>
+	<p>&nbsp;</p>
+	<?php
+		include("nav.html");
+	?>
+
+</div>
+
+</body>
+</html>
